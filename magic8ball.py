bc41fb52cdc74a2dd3a6241595f3225230cb753f
Eric Campbell <soupyc@gmail.com>
2016-04-08 00:33:48 -0700

    Comment

---
+++ magic8ball.py
@@ -0,0 +1,10 @@
+# I'm sure you've used a magic 8 ball at one point in your life. You ask it a question, turn it right side up
+# and it gives an answer by way of a floating die with responses written on it. You can create one in python. You must:
+# Allow the user to enter their question
+# Display an in progress message( i.e. "thinking")
+# Create 20 responses, and show a random response
+# Allow the user to ask another question or quit
+# Bonus Using whatever module you like, add a gui. Your gui must have:
+# A box for users to enter the question
+# At least 4 buttons: Ask , clear(the text box), play again and quit(this must close the window)
+
