50a062098dfd2ddc01fd895fb776417220a08881
Eric Campbell <soupyc@gmail.com>
2016-04-12 17:45:31 -0700

    Code push 1

--- magic8ball.py
+++ magic8ball.py
@@ -8,3 +8,30 @@
 # A box for users to enter the question
 # At least 4 buttons: Ask , clear(the text box), play again and quit(this must close the window)
 
+import random
+import time
+
+eight_ball = [ "It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely",
+               "You may rely on it", "As I see it, yes", "Most Likely", "Outlook Good",
+               "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later",
+               "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+               "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very Doubtful"]
+
+def question():
+    question = raw_input("You may ask your yes or no question of the Magic 8 Ball!\n")
+    print("Thinking...")
+    time.sleep(random.randrange(0,5))
+    print(random.choice(eight_ball))
+
+question()
+
+repeat = raw_input("Would you like to ask another question? (Y or N)")
+if repeat == "y" or repeat == "Y":
+    question()
+else:
+    print "Come back if you have more questions!"
+
+
+
+
+
